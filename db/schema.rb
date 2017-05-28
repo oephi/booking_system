7def01a80b3d85cf2d0e1e8091a1143d9aee97a3
jbat <jonathon.batson@gmail.com>
2017-05-28 12:38:38 +1000

    Create Develop Branch, adjusts gemfile, removes enrolements table??

--- db/schema.rb
+++ db/schema.rb
@@ -31,15 +31,6 @@ ActiveRecord::Schema.define(version: 20170520012241) do
     t.index ["student_id"], name: "index_courses_students_on_student_id"
   end
 
-  create_table "enrolments", force: :cascade do |t|
-    t.integer  "student_id"
-    t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_enrolments_on_course_id"
-    t.index ["student_id"], name: "index_enrolments_on_student_id"
-  end
-
   create_table "locations", force: :cascade do |t|
     t.string   "rooms"
     t.string   "name"
